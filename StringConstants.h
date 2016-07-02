7668750005422a17c8ae6c769ad37b0fa2618b07
Igor Vasilenko <ivasilenko@yotateam.com>
2016-07-02 15:07:35 +0300

    Added StringConstants file

---
+++ StringConstants.h
@@ -0,0 +1,7 @@
+//
+// Created by Igor Vasilenko on 02/07/16.
+// Copyright (c) 2016 Igor Vasilenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
---
+++ StringConstants.m
@@ -0,0 +1,8 @@
+//
+// Created by Igor Vasilenko on 02/07/16.
+// Copyright (c) 2016 Igor Vasilenko. All rights reserved.
+//
+
+#import "StringConstants.h"
+
+
