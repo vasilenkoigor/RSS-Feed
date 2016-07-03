c69f8f5b9538de520d732e0500d7e4be413fe836
Igor Vasilenko <ivasilenko@yotateam.com>
2016-07-03 14:33:50 +0300

    Implemented delegateForTableView:baseTableViewDelegate: for FeedDataDisplayManager

--- RSS Feed/Classes/PresentationLayer/UserStories/FeedUserStory/Modules/FeedModule/View/FeedDataDisplayManager/FeedDataDisplayManager.m
+++ RSS Feed/Classes/PresentationLayer/UserStories/FeedUserStory/Modules/FeedModule/View/FeedDataDisplayManager/FeedDataDisplayManager.m
@@ -6,7 +6,7 @@
 #import <Nimbus/NimbusModels.h>
 #import "FeedDataDisplayManager.h"
 
-@interface FeedDataDisplayManager ()
+@interface FeedDataDisplayManager () <UITableViewDelegate>
 
 @property (strong, nonatomic) NITableViewModel *tableViewModel;
 @property (strong, nonatomic) NSArray *cellObjects;
@@ -15,12 +15,20 @@
 
 @implementation FeedDataDisplayManager
 
+#pragma mark - DataDisplayManager protocol
+
 - (id <UITableViewDataSource>)dataSourceForTableView:(UITableView *)tableView
 {
     if (!self.tableViewModel) {
         self.tableViewModel = [self configureTableViewModel];
     }
     return self.tableViewModel;
+}
+
+- (id <UITableViewDelegate>)delegateForTableView:(UITableView *)tableView baseTableViewDelegate:(id <UITableViewDelegate>)baseTableViewDelegate
+{
+    NITableViewActions *tableViewActions = [[NITableViewActions alloc] initWithTarget:self];
+    return [tableViewActions forwardingTo:self];
 }
 
 #pragma mark - Private
